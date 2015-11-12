1f54aa8ab3f80d94466192b0ef8ebb97df40af3c
rcs333 <ryanshean1@gmail.com>
2015-11-12 14:03:19 -0800

    Quick quality of life fixes as well as some new functions
    Added capturing of unknown words, added word stemming with built in NTLK
    stemmer, reordered all of the functions in a way that makes sense and
    added the thing that lets this work as a module

--- Word Analysis.py
+++ Word Analysis.py
@@ -1,19 +1,35 @@
 __author__ = 'Ryan Shean'
 
-#TODO: Examine the efficeincy and complexity of what i'm doing here, im pretty sure its O(N) but idk
 #TODO: Try to fix the arbitary assignment of synsets and hypernyms
-#TODO: Maybe work on nicer output, graphs w/ matlabplot lib ?
+#TODO: graphs w/ matlabplot lib ?
+#TODO: Capture the unknown words for posterity
+#TODO: update the variable names to not have typing
 
 import nltk
 from nltk.corpus import wordnet
 
 #function that tokenizes a file given a path and returns a list of all tokens
-#as strings
+#as lemmatized strings
 def read_lemmas(path):
     lemmas = []
+    tokens = []
     for word in open(path).read().split():
-        lemmas.append(word)
+        tokens.append(word)
+    lemmas = [nltk.WordNetLemmatizer().lemmatize(t) for t in tokens]
     return lemmas
+
+#Takes a list of text strings and returns a list of snyset object coresponding to
+#the strings, duplicates are allowed and strings that do not have synsets are
+#put in another list and returned as a tuple(?) that needs to be indexed to grab
+def make_synset(list):
+    syn_list = []
+    unknown_words = []
+    for word in list:
+        try:
+            syn_list.append(wordnet.synsets(word)[0])
+        except IndexError:
+            unknown_words.append(word)
+    return syn_list,unknown_words
 
 #Takes a list of synsnet objects and creates and returns a list of all hypernyms
 #inside of the list, duplicates are allowed and the list is completely unsorted
@@ -26,22 +42,19 @@ def make_hyper_list(list):
             pass
     return hyper_list
 
-#Takes a list of text strings and returns a list of snyset object coresponding to
-#the strings, duplicates are allowed and strings that do not have synsets are dropped
-def make_synset(list):
-    syn_list = []
-    for word in list:
-        try:
-            syn_list.append(wordnet.synsets(word)[0])
-        except IndexError:
-            pass
-    return syn_list
-
-#Takes a map of keys and values and prints them in decending orderd
-def sort_print(map):
-    for w in sorted(map, key=map.get, reverse=True):
-        print(w, map[w])
-    print("\n\n\n")
+#takes a list of words and returns a map containing parts of speech and their counts
+#Had to be hardcoded because POS tagging output tuples(?) and I didn't want to have to deal
+#with generalizing the other function
+def tag_pos(list):
+    pos_tagged = nltk.pos_tag(list)
+    pos_map = {}
+    for item in pos_tagged:
+        pos = item[1]
+        if pos in pos_map:
+            pos_map[pos] += 1
+        else:
+            pos_map[pos] = 1
+    return pos_map
 
 #Takes a list of any object and counts them and creates a map
 #probobly inefficient but I'm not working with an extremly large data set
@@ -54,39 +67,53 @@ def create_counts(list):
             counts[item] = 1
     return counts
 
-#Open file and tokenize into strings
-lemmas = read_lemmas("allwords.txt")
+#Takes a map of keys and values and prints them in decending orderd
+def sort_print(map):
+    for w in sorted(map, key=map.get, reverse=True):
+        #Hardcoded tolerance, should probobly fix by passing as argument
+        if map[w] > 3:
+            print(w, map[w])
+    print("\n\n\n")
 
-#match all the words to a (somewhat arbitrary) wordnet definition
-#and then count them
-synset = make_synset(lemmas)
-synset_counts = create_counts(synset)
 
-#Create a list of hypernyms off the "found" words in the file
-hypers_1 = make_hyper_list(synset)
-hypers_counts_1 = create_counts(hypers_1)
 
-#Create two lists of hypernyms one "level" up each
-hypers_2 = make_hyper_list(hypers_1)
-hypers_counts_2 = create_counts(hypers_2)
+#I think this allows this code to be imported as a module
+if __name__ == '__main__':
+    #Open file and tokenize into strings
+    lemmas = read_lemmas("allwords.txt")
 
-hypers_3 = make_hyper_list(hypers_2)
-hypers_counts_3 = create_counts(hypers_3)
+    #match all the words to a (somewhat arbitrary) wordnet definition
+    #and then count them grab the known words and unknowns with
+    #AUTOMATIC UNPACKING HOLY SHIT THATS COOL
+    synset,unknowns = make_synset(lemmas)
+    synset_counts = create_counts(synset)
 
-#Tag all of the words and then count each occurance of each part of speech
-pos_tagged = nltk.pos_tag(lemmas)
-pos_map = {}
-for item in pos_tagged:
-    pos = item[1]
-    if pos in pos_map:
-        pos_map[pos] += 1
-    else:
-        pos_map[pos] = 1
+    #Create a list of hypernyms off the "found" words in the file
+    hypers_1 = make_hyper_list(synset)
+    hypers_counts_1 = create_counts(hypers_1)
 
-#Print all dat shit
-sort_print(pos_map)
-print("\n\n\n")
-sort_print(synset_counts)
-sort_print(hypers_counts_1)
-sort_print(hypers_counts_2)
-sort_print(hypers_counts_3)
+    #Create two lists of hypernyms one "level" up each
+    hypers_2 = make_hyper_list(hypers_1)
+    hypers_counts_2 = create_counts(hypers_2)
+
+    hypers_3 = make_hyper_list(hypers_2)
+    hypers_counts_3 = create_counts(hypers_3)
+
+    hypers_4 = make_hyper_list(hypers_3)
+    hypers_counts_4 = create_counts(hypers_4)
+
+
+
+    #Print all dat shit
+    sort_print(tag_pos(lemmas))
+    print("\n\n\n")
+    print("Words Detected:")
+    sort_print(synset_counts)
+    print("First Level of Hypernyms:")
+    sort_print(hypers_counts_1)
+    print("Second Level of Hypernyms:")
+    sort_print(hypers_counts_2)
+    print("Third Level of Hypernyms:")
+    sort_print(hypers_counts_3)
+    print("Fourth Level of Hypernyms:")
+    sort_print(hypers_counts_4)
